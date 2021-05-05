9190c2231eb2b8efb01381770ceb3288d94388dd
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2021-05-05 11:16:57 +0900

    fix code

--- src/main/java/com/hamusuke/flycommod/Main.java
+++ src/main/java/com/hamusuke/flycommod/Main.java
@@ -5,7 +5,6 @@ import com.hamusuke.flycommod.command.CommandFlying;
 import com.hamusuke.flycommod.item.ItemFlyingStick;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
