69ee29534fc479d28c00ef6020bc9d4ad84ba50f
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2021-05-14 20:54:20 +0900

    Made setFlySpeed and setWalkSpeed usable on server

--- src/main/java/com/hamusuke/flycommod/command/CommandEntityAbilities.java
+++ src/main/java/com/hamusuke/flycommod/command/CommandEntityAbilities.java
@@ -10,20 +10,33 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.TranslatableText;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class CommandEntityAbilities {
 	public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-		LiteralArgumentBuilder<ServerCommandSource> literalargumentbuilder = CommandManager.literal("entityabilities").requires((permission) -> permission.hasPermissionLevel(2));
+		LiteralArgumentBuilder<ServerCommandSource> literalArgumentBuilder = CommandManager.literal("entityabilities").requires((permission) -> permission.hasPermissionLevel(2));
 
-		literalargumentbuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("noGravity").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> noGravity(command.getSource(), EntityArgumentType.getEntities(command, "targets"), BoolArgumentType.getBool(command, "boolean"))))));
+		literalArgumentBuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("noGravity").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> {
+			boolean flag = BoolArgumentType.getBool(command, "boolean");
+			return noGravity(command.getSource(), EntityArgumentType.getEntities(command, "targets").stream().filter(entity -> entity.hasNoGravity() != flag).collect(Collectors.toList()), flag);
+		}))));
 
-		literalargumentbuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("setGlowing").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> setGlowing(command.getSource(), EntityArgumentType.getEntities(command, "targets"), BoolArgumentType.getBool(command, "boolean"))))));
+		literalArgumentBuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("setGlowing").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> {
+			boolean flag = BoolArgumentType.getBool(command, "boolean");
+			return setGlowing(command.getSource(), EntityArgumentType.getEntities(command, "targets").stream().filter(entity -> entity.isGlowing() != flag).collect(Collectors.toList()), flag);
+		}))));
 
-		literalargumentbuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("setInvulnerable").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> setInvulnerable(command.getSource(), EntityArgumentType.getEntities(command, "targets"), BoolArgumentType.getBool(command, "boolean"))))));
+		literalArgumentBuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("setInvulnerable").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> {
+			boolean flag = BoolArgumentType.getBool(command, "boolean");
+			return setInvulnerable(command.getSource(), EntityArgumentType.getEntities(command, "targets").stream().filter(entity -> entity.isInvulnerable() != flag).collect(Collectors.toList()), flag);
+		}))));
 
-		literalargumentbuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("setInvisible").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> setInvisible(command.getSource(), EntityArgumentType.getEntities(command, "targets"), BoolArgumentType.getBool(command, "boolean"))))));
+		literalArgumentBuilder.then(CommandManager.argument("targets", EntityArgumentType.entities()).then(CommandManager.literal("setInvisible").then(CommandManager.argument("boolean", BoolArgumentType.bool()).executes((command) -> {
+			boolean flag = BoolArgumentType.getBool(command, "boolean");
+			return setInvisible(command.getSource(), EntityArgumentType.getEntities(command, "targets").stream().filter(entity -> entity.isInvisible() != flag).collect(Collectors.toList()), flag);
+		}))));
 
-		dispatcher.register(literalargumentbuilder);
+		dispatcher.register(literalArgumentBuilder);
 	}
 
 	private static int noGravity(ServerCommandSource source, Collection<? extends Entity> entities, boolean flag) {
@@ -36,7 +49,7 @@ public class CommandEntityAbilities {
 
 		if (entities.size() == 1) {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.nogravity." + flag + ".single", entities.iterator().next().getDisplayName()), true);
-		} else if (entities.size() > 1) {
+		} else {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.nogravity." + flag + ".multiple", entities.size()), true);
 		}
 
@@ -48,7 +61,7 @@ public class CommandEntityAbilities {
 
 		if (entities.size() == 1) {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.setglowing." + flag + ".single", entities.iterator().next().getDisplayName()), true);
-		} else if (entities.size() > 1) {
+		} else {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.setglowing." + flag + ".multiple", entities.size()), true);
 		}
 
@@ -60,7 +73,7 @@ public class CommandEntityAbilities {
 
 		if (entities.size() == 1) {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.setinvulnerable." + flag + ".single", entities.iterator().next().getDisplayName()), true);
-		} else if (entities.size() > 1) {
+		} else {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.setinvulnerable." + flag + ".multiple", entities.size()), true);
 		}
 
@@ -72,7 +85,7 @@ public class CommandEntityAbilities {
 
 		if (entities.size() == 1) {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.invisible." + flag + ".single", entities.iterator().next().getDisplayName()), true);
-		} else if (entities.size() > 1) {
+		} else {
 			source.sendFeedback(new TranslatableText("hamusuke.command.entityabilities.success.invisible." + flag + ".multiple", entities.size()), true);
 		}
 
---
+++ src/main/java/com/hamusuke/flycommod/invoker/PlayerAbilitiesInvoker.java
@@ -0,0 +1,7 @@
+package com.hamusuke.flycommod.invoker;
+
+public interface PlayerAbilitiesInvoker {
+    void setFlySpeed(float flySpeed);
+
+    void setWalkSpeed(float walkSpeed);
+}
