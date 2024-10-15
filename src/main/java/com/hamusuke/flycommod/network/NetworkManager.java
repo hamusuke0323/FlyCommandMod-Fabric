724df88179451b98108b705767b24296d469ca25
Cassian Godsted <cassian@godsted.com>
2024-10-14 23:18:06 -0400

    Port to 1.21

--- src/main/java/com/hamusuke/flycommod/network/NetworkManager.java
+++ src/main/java/com/hamusuke/flycommod/network/NetworkManager.java
@@ -1,8 +1,15 @@
 package com.hamusuke.flycommod.network;
 
 import com.hamusuke.flycommod.FlyCommandMod;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Uuids;
+
+import java.util.UUID;
 
 public final class NetworkManager {
-    public static final Identifier NO_FALL_MARK_PACKET = new Identifier(FlyCommandMod.MOD_ID, "no_fall_mark_packet");
+    public static final Identifier NO_FALL_MARK_PACKET = Identifier.of(FlyCommandMod.MOD_ID, "no_fall_mark_packet");
+
 }
